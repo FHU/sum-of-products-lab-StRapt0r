50469326c4fc7b4511934f57c6777a1a98775580
Elijah <87155048+StRapt0r@users.noreply.github.com>
2024-02-06 08:06:27 -0800

    Fixed formatting

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 def sum_of_products(list1, list2):
-    output=((list1[0]*list2[0])+(list1[1]*list2[1])+(list1[2]*list2[2]))
+    output=((int(list1[0])*int(list2[0]))+(int(list1[1])*int(list2[1]))+(int(list1[2])*int(list2[2])))
     return(output)
 
 
 if __name__ == '__main__':
-   a=([int(input("Input an interger here:")),int(input("Input an interger here:")),int(input("Input an interger here:"))])
-   b=([int(input("Input an interger here:")),int(input("Input an interger here:")),int(input("Input an interger here:"))])
+   a=(input())
+   b=(input())
+   a = a.split()
+   b = b.split()
    x= sum_of_products(a,b)
    print("The answer is:", x)
