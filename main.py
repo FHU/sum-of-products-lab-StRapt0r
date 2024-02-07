6623cd4f544a69f616d1b06661368372d0278f62
Elijah <87155048+StRapt0r@users.noreply.github.com>
2024-02-06 20:16:20 -0800

    Ok hopefuly done

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def sum_of_products(list1, list2):
-    output=((int(list1[0])*int(list2[0]))+(int(list1[1])*int(list2[1]))+(int(list1[2])*int(list2[2])))
+    output=0
+    for i in (list1):
+        output=output+(list1[i-1]*list2[i-1])
     return(output)
 
 
@@ -8,5 +10,7 @@ if __name__ == '__main__':
    b=(input())
    a = a.split()
    b = b.split()
+   a = [eval(i) for i in a]
+   b = [eval(i) for i in b]
    x= sum_of_products(a,b)
    print(x)
