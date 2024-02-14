f22592cbb2f99ed28aeb90966695e73272c996b6
Elijah <87155048+StRapt0r@users.noreply.github.com>
2024-02-14 12:09:27 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 def sum_of_products(list1, list2):
     output=0
+    a = [int(i) for i in list1.split(' ')]
+    b = [int(i) for i in list2.split(' ')]
     for i in (list1):
         output=output+(list1[i-1]*list2[i-1])
     return(output)
 
 
 if __name__ == '__main__':
-   a=(input())
-   b=(input())
-   a = a.split()
-   b = b.split()
-   a = [eval(i) for i in a]
-   b = [eval(i) for i in b]
+   a=input()
+   b=input()
    x= sum_of_products(a,b)
    print(x)
